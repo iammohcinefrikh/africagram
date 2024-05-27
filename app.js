354119f1837e3a43953c08df825b2d1cc03f598e
Mohcine Frikh <iammohcinefrikh@hotmail.com>
2024-05-27 10:23:29 +0100

    modified package.json file

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express=require("express");
+const express = require("express");
 require("dotenv").config();
 
-const app=express();
+const app = express();
 
-const port=process.env.APP_PORT || 8080;
+const port = process.env.APP_PORT || 8080;
 
 app.listen(port, ()=>{
   console.log(`Server is running on PORT ${port}.`);
