795844d53bf522fa0a31bd8bb3abe4cfd1776c68
Mohcine Frikh <iammohcinefrikh@hotmail.com>
2024-05-27 09:22:00 +0100

    added initial project files

---
+++ app.js
@@ -0,0 +1,10 @@
+const express=require("express");
+require("dotenv").config();
+
+const app=express();
+
+const port=process.env.APP_PORT || 8080;
+
+app.listen(port, ()=>{
+  console.log(`Server is running on PORT ${port}.`);
+});
