5d03d96379f51307fd31cbb38c5d9f972dcfe9f3
Mohcine Frikh <iammohcinefrikh@hotmail.com>
2024-05-28 09:57:37 +0100

    added authentificationRouter.js code

--- routes/authentificationRouter.js
+++ routes/authentificationRouter.js
@@ -1,1 +1,10 @@
 const express = require("express");
+
+const { register, login } = require("../controllers/authentificationController.js");
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+
+module.exports = router;
