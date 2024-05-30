f0d50215eff4007cf15429e581052f773013a1ad
youssef-elhoubi <elhoubiyoussef@gmail.com>
2024-05-30 14:16:52 +0100

    add the user update acount end point

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const authentificationRoutes = require("./routes/authentificationRouter.js");
 const postRoutes = require("./routes/postRouter.js");
 const coumentRouter= require("./routes/commentRouter.js");
+const userRouter= require("./routes/userRouter.js");
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(authentificationRoutes);
 app.use(postRoutes);
 app.use(coumentRouter);
+app.use(userRouter);
 
 const port = process.env.APP_PORT || 8080;
 
